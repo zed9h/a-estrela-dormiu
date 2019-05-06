c325890f25d77e71da73c64092b5012d8c2d660c
Carlo "zED" Caputo <carlo.caputo@gmail.com>
2019-05-05 23:07:31 -0300

    camera transform

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float renderScale = 1.4f;
+
+    public void Recenter() {
+        UnityEngine.XR.InputTracking.Recenter();
+    }
+
+    void Start()
+    {
+        UnityEngine.XR.XRSettings.eyeTextureResolutionScale = renderScale;
+        Recenter();
+    }
+
+}
