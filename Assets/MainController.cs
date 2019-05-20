672dc445e8840b454c9ac58decd91b02659da71a
Carlo Caputo <zed@z2.lan>
2019-05-20 01:39:54 -0300

    unity downgrade and tons of fixes

---
+++ Assets/MainController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class MainController : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public float renderScale = 1.4f;
+    public Transform cam;
+    public Text log;
+
+    public void Recenter() {
+        InputTracking.Recenter();
+    }
+
+    void Start()
+    {
+        UnityEngine.XR.XRSettings.eyeTextureResolutionScale = renderScale;
+
+        InputTracking.disablePositionalTracking = true;
+        XRDevice.SetTrackingSpaceType(TrackingSpaceType.Stationary);
+        cam.localPosition = Vector3.zero;
+        Recenter();
+
+        if (timeline == null) timeline = GetComponentInParent<PlayableDirector>();
+    }
+
+    private void Update() {
+        if(Input.GetMouseButton(0) || Input.anyKeyDown) {
+            Recenter();
+            timeline.time = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Recenter();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# A Estrela Dormiu
-* Elieni Caputo: Texto, Narração e Direção
-* Carlo Caputo: Modelagem e Programação
-* Ana Ribeiro: Animações e Programação
+# POEM
+```
+Hoje o dia caiu sem esperança
+A estrela Dormiu
+Bebeu
+Minha lágrima
+Chorosa
+E diamante
+Ei-la orvalhando a mão
+Que a língua lambe
+```
 
+# CREDITS
+
+* Elieni Caputo
+* Carlo Caputo
+* Ana Ribeiro
+
+
+# TODO
+
+* Video capture
+
+
+# CHARSET
+
+* ` -ABCDEHMQabcdeghijlmnoprstuváãçí`
+* ` -+/*!@#$%~'"&.,:;!?abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáãéíóõúçÁÃÉÍÓÕÚÇ1234567890()[]{}`
